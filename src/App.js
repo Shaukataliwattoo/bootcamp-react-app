d60af5ede98e41953ff625daa44c18e42b7a00e6
Shaukat Ali <shaukatwattoo921@gmail.com>
2020-06-06 23:25:58 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World from Shaukat Ali and Muhammad Ahmed
+    </div>
+  );
+}
+
+export default App;
