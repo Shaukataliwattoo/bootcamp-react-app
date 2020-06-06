1d10416df95cea720bf4047d6c943480647105a9
Shaukat Ali <shaukatwattoo921@gmail.com>
2020-06-07 00:51:56 +0500

    display message again updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World from Shaukat Ali with github action</h1>
-      <h2>Second Message</h2>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Hello React World from Shaukat Ali
+        </h2>
+        <p>
+          Auto Deployment on Surge through Github actions with custome YAML config.
+        </p>
+        <a
+          className="App-link"
+          href="https://mi3afzal.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact me Here
+        </a>
+      </header>
     </div>
   );
 }
